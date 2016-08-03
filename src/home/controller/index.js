46c03a4ed13381bb509bdae373af51b83804a5b2
cityhunterhua <18573485@qq.com>
2016-08-03 18:08:55 +0800

    数组

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -7,12 +7,24 @@ module.exports = think.controller(Base, {
    * index action
    * @return {Promise} []
    */
-  init:function(http){
-     this.super("init", http);
-     this.assign("desc","这里是正文内容~");
+  __before(){
+      this.assign("before","前置操作~");
   },
+   init:function(http){
+      this.super("init", http);
+      this.assign("desc","这里是正文内容~");
+   },
     indexAction: function(self){
-     this.assign("title","这里是首页~");
-     return self.display();
-  }
+      let http = this.http;
+      let arr = new Array(11,22,333);
+      let params = this.get();
+       console.log(params);console.log(arr);
+      this.assign("params",arr);
+      this.assign("title","这里是首页~");
+      return self.display();
+   },
+   apiAction:function(){
+       let data = {name: "thinkjs"};
+       this.success(data);
+   }
 });
