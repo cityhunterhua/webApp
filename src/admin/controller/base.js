21d6d38eb782d5fa578f6d3536b8460fee4c66b9
cityhunterhua <18573485@qq.com>
2016-08-10 17:28:45 +0800

    后台页面上传

--- src/admin/controller/base.js
+++ src/admin/controller/base.js
@@ -4,4 +4,20 @@ export default class extends think.controller.base {
   /**
    * some base method in here
    */
+  async __before(){
+    await this.getConfig();
+    let csrf=await this.session("__CSRF__");
+    this.assign("csrf",csrf);
+    let userinfo=await this.session("userInfo");
+    if(think.isEmpty(userinfo)&&this.http.controller!="login"){
+      return this.redirect("/admin/login");
+    }else{
+      this.assign('userinfo',userinfo);
+    }
+  }
+  async getConfig(){
+    let sysinfo = await this.model("system").limit(1).find();
+    this.assign("_STATIC",sysinfo.url+"static/");
+    this.assign("_sys",sysinfo);
+  }
 }
---
+++ src/admin/controller/login.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import Base from './base.js';
+
+export default class extends Base {
+  /**
+   * index action
+   * @return {Promise} []
+   */
+  indexAction(){
+    //auto render template file index_index.html
+    return this.display();
+  }
+  doLoginAction(){
+      let data=this.post();
+      let md5Pas = await think.md5(data.password);
+      let uname = await data.username;
+      let result=await this.model("user").where({name:uname,role:{">":0} }).find();
+      let info={
+        name: uname,
+        password: md5Pas
+      }
+      if(uname===result.name&&md5Pas===result.password){
+        await this.session("userInfo", info);
+        return this.json({status:1,msg:"登陆成功!"});
+      }else{
+        return this.json({status:0,msg:"用户名或密码错误!"});
+      }
+  }
+}
